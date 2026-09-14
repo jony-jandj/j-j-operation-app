08dfeb41b73ec4c12e0de951f94cbd8f58aa1bb2
jony-jandj <jony@jandjhomerenovations.com>
2026-09-13 19:28:09 -0500

    Show Purchased by directly on selection cards

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jj-operations-v57-bulk-selections';
+const CACHE_NAME = 'jj-operations-v58-purchased-by-cards';
 const APP_SHELL = ['./','./index.html','./jj-original-logo.png','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
