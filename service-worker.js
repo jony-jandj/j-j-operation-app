49ff13dc1c1530d49871d21ba6cba0aa68887397
jony-jandj <jony@jandjhomerenovations.com>
2026-09-17 22:42:35 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jj-operations-work-pay-20260917-4';
+const CACHE_NAME = 'jj-operations-work-pay-20260917-5';
 const APP_SHELL = [
   './work-pay.js','./estimate-full-parser.js',
   './','./index.html','./homeowner.html','./customer-view.js','./customer-overlay.js','./qrcode.min.js','./selections-metadata.js',
